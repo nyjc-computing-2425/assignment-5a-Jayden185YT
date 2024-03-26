6ffd0c7a8d4423a18a57a3ea15bcf518220369f7
Jayden Tiew <106733343+Jayden185YT@users.noreply.github.com>
2024-03-26 04:28:40 +0000

    Final Submission

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 def to_hms(seconds: int) -> list:
-    """
-    Converts seconds to hours, minutes, and seconds, and returns it as a list.
+  """
+  Converts seconds to hours, minutes, and seconds, and returns it as a list.
 
-    Parameters
-    ---------
-    seconds: int
-        the seconds to be calculated
+  Parameters
+  ---------
+  seconds: int
+      the seconds to be calculated
 
-    Returns
-    ---------
-    list
-        a list of integers representing hours, minutes, seconds
+  Returns
+  ---------
+  list
+      a list of integers representing hours, minutes, seconds
 
-    Example:
-    >>> to_hms(10)
-    [0, 0, 10]
-    >>> to_hms(61)
-    [0, 1, 1]
-    >>> to_hms(7199)
-    [1, 59, 59]
-    """
-    # Type your code below
-    pass
+  Example:
+  >>> to_hms(10)
+  [0, 0, 10]
+  >>> to_hms(61)
+  [0, 1, 1]
+  >>> to_hms(7199)
+  [1, 59, 59]
+  """
+  # Type your code below
+  result = []
+  if not isinstance(seconds, int):
+    print("Unsupported input type.")
+    return result
+  result = [0,0,0]
+  hours, minutes = divmod(seconds, 3600)
+  result[0] = hours
+  seconds %= 60
+  minutes //= 60
+  result[1] = minutes
+  result[2] = seconds
+  return result
 
